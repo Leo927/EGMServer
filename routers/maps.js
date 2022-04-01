89909aacb147c97357f75d991d4215a4880dc1b2
Songhao <songhao.li@hotmail.com>
2022-04-01 10:53:59 -0600

    formated maps

--- routers/maps.js
+++ routers/maps.js
@@ -11,118 +11,117 @@ const { getUser } = require("../authentication/github");
 const router = express.Router();
 
 // pass route to next handler
-router.use((req, ress, next)=>{
-    console.log("Time:", Date.now());
-    next();
+router.use((req, ress, next) => {
+  console.log("Time:", Date.now());
+  next();
 });
 
 // update an existing map
-router.patch('/', async (req, res)=>{
-    try{
-        const {token, mapId, mapData} = req.body;
-        const user = await getUser(token);
-        const map = await mapDb.getMap(mapId);
-        if(user.uid != map.uid){
-            res.sendStatus(400);
-            return;
-        }
-        await mapDb.updateMap(req.body.mapId, mapData);
-        console.log(`Map updated. ${await mapDb.getMap(mapId)}`);
-        res.send({mapId});
+router.patch('/', async (req, res) => {
+  try {
+    const { token, mapId, mapData } = req.body;
+    const user = await getUser(token);
+    const map = await mapDb.getMap(mapId);
+    if (user.uid != map.uid) {
+      res.sendStatus(400);
+      return;
     }
-    catch(e){
-        res.sendStatus(400);
-        console.error(e);
-    }
+    await mapDb.updateMap(req.body.mapId, mapData);
+    console.log(`Map updated. ${await mapDb.getMap(mapId)}`);
+    res.send({ mapId });
+  }
+  catch (e) {
+    res.sendStatus(400);
+    console.error(e);
+  }
 });
 
 // get map by user id
-router.get('/owner/:uid', async(req,res)=>{
-    try{
-        const {uid} = req.params;
-        const maps = await mapDb.getUserMaps(uid);
-        res.send(maps);
-    }catch(e){
-        res.sendStatus(400);
-        console.error(e);
-    }
+router.get('/owner/:uid', async (req, res) => {
+  try {
+    const { uid } = req.params;
+    const maps = await mapDb.getUserMaps(uid);
+    res.send(maps);
+  } catch (e) {
+    res.sendStatus(400);
+    console.error(e);
+  }
 })
 
 // insert a new map
-router.post('/', async (req, res)=>{
-    if(!req.body?.token){
-        res.sendStatus(400);
-        return;
+router.post('/', async (req, res) => {
+  if (!req.body?.token) {
+    res.sendStatus(400);
+    return;
+  }
+  try {
+    const user = await getUser(req.body.token);
+    if (!user) {
+      res.sendStatus(403);
+      return;
     }
-    try{
-        const user = await getUser(req.body.token);
-        if(!user)
-        {
-            res.sendStatus(403);
-            return;
-        }
-        const mapData = {...req.body.mapData};
-        mapData.uid = user.id.toString();
-        if(mapData._id != undefined)
-            delete mapData._id;
-        const mapId = await mapDb.insertMap(mapData);
-        console.log(`New map Created. id:${mapId}`);
-        res.send({mapId});
-    }
-    catch(e){
-        console.error(e);
-        res.sendStatus(500);
-    }
+    const mapData = { ...req.body.mapData };
+    mapData.uid = user.id.toString();
+    if (mapData._id != undefined)
+      delete mapData._id;
+    const mapId = await mapDb.insertMap(mapData);
+    console.log(`New map Created. id:${mapId}`);
+    res.send({ mapId });
+  }
+  catch (e) {
+    console.error(e);
+    res.sendStatus(500);
+  }
 });
 
 // get map by mapId
-router.get('/:mapId', async (req,res)=>{
-    try{
-        const {mapId} = req.params;
-        const map = await mapDb.getMap(mapId);
-        if(!map){
-            res.send(404);
-            return;
-        }
-        res.send(map);
+router.get('/:mapId', async (req, res) => {
+  try {
+    const { mapId } = req.params;
+    const map = await mapDb.getMap(mapId);
+    if (!map) {
+      res.send(404);
+      return;
     }
-    catch(e){
-        res.sendStatus(500);
-        console.error(e);
-    }
+    res.send(map);
+  }
+  catch (e) {
+    res.sendStatus(500);
+    console.error(e);
+  }
 });
 
 // get map listing
 router.get('/', async (req, res) => {
-    try{
-        res.send(await mapDb.getMapListing());
-    }
-    catch(e){
-        res.sendStatus(500);
-        console.error(e);
-    }
+  try {
+    res.send(await mapDb.getMapListing());
+  }
+  catch (e) {
+    res.sendStatus(500);
+    console.error(e);
+  }
 });
 
 // delete map by map id
-router.delete('/', async (req, res)=>{
-    try{
-        const {token, mapId} = req.body;
-        if(!token || !mapId){
-            res.sendStatus(400);            
-            return;
-        }   
-        const user = await getUser(token);
-        const map = await mapDb.getMap(mapId);
-        if(user.uid != map.uid){
-            res.sendStatus(403);
-            return;
-        }
-        res.send(await mapDb.deleteMap(mapId));
+router.delete('/', async (req, res) => {
+  try {
+    const { token, mapId } = req.body;
+    if (!token || !mapId) {
+      res.sendStatus(400);
+      return;
     }
-    catch(e){
-        res.sendStatus(500);
-        console.error(e);
+    const user = await getUser(token);
+    const map = await mapDb.getMap(mapId);
+    if (user.uid != map.uid) {
+      res.sendStatus(403);
+      return;
     }
+    res.send(await mapDb.deleteMap(mapId));
+  }
+  catch (e) {
+    res.sendStatus(500);
+    console.error(e);
+  }
 });
 
 module.exports = router;
